1765be1d333f8931195e62ffdca0372a753777a0
SW1073 <eloiimerino@gmail.com>
2025-10-24 09:13:48 +0200

    feat: randomized input generation for memory_controller_tb

--- tb/common_tb/memory_controller_tb.sv
+++ tb/common_tb/memory_controller_tb.sv
@@ -55,28 +55,52 @@ module tb (
         @(posedge reset_n);
         @(posedge clk);
 
-        write(32'h00000100, MEMOP_WIDTH_32, 32'hcac0cafe,
-            valid_i, we_i, addr_i, data_i, width_i);
-        @(posedge clk);
+        // test_directed();
+        test_random(100, 50, 32'h0000_1000);
 
+        noop(valid_i, we_i, addr_i, data_i, width_i);
+        @(posedge clk);
+        $finish;
+    end
+
+    task test_random(input int n_ops, input int write_prob, input logic[XLEN-1:0] addr_base);
+        logic [3:0] addr_offset;
+        logic [XLEN-1:0] data;
+        memop_width_e width;
+        for (int i = 0; i < n_ops; ++i) begin
+            // Randomize
+            addr_offset = $urandom_range(0, 1<<4);
+            data = $urandom_range(0, 1<<32);
+            width = memop_width_e'($urandom_range(0, 1<<2));
+            if ($urandom_range(1,100) < write_prob) begin
+                write(addr_base+addr_offset, width, data,
+                    valid_i, we_i, addr_i, data_i, width_i);
+            end else begin
+                read(addr_base+addr_offset, width,
+                    valid_i, we_i, addr_i, data_i, width_i);
+            end
+            @(posedge clk);
+        end
+    endtask
+
+    task test_directed();
+        @(posedge clk);
         write(32'h00000105, MEMOP_WIDTH_8, 32'h000000ca,
             valid_i, we_i, addr_i, data_i, width_i);
         @(posedge clk);
-
+        write(32'h00000100, MEMOP_WIDTH_32, 32'hcac0cafe,
+            valid_i, we_i, addr_i, data_i, width_i);
+        @(posedge clk);
         read(32'h00000100, MEMOP_WIDTH_16,
             valid_i, we_i, addr_i, data_i, width_i);
         @(posedge clk);
-
         read(32'h00000102, MEMOP_WIDTH_16,
             valid_i, we_i, addr_i, data_i, width_i);
         @(posedge clk);
-
         read(32'h00000104, MEMOP_WIDTH_16,
             valid_i, we_i, addr_i, data_i, width_i);
         @(posedge clk);
-
-        $finish;
-    end
+    endtask
 
     task read (
         input logic[XLEN-1:0] addr,
