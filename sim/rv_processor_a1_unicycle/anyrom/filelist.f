405efb54a956e05f819cf03e814419d6d3adc9fa
s4izh <sergiosanz234@gmail.com>
2025-10-26 18:35:10 +0100

    refactor: rename anyrom testbench to tb_anyrom

--- sim/rv_processor_a1_unicycle/anyrom/filelist.f
+++ sim/rv_processor_a1_unicycle/anyrom/filelist.f
@@ -3,5 +3,5 @@
 $(PROJ_DIR)/rtl/common/rom.sv
 $(PROJ_DIR)/rtl/common/sram.sv
 
-$(PROJ_DIR)/tb/rv_processor_a1_unicycle/tb.sv
+$(PROJ_DIR)/tb/rv_processor_a1_unicycle/tb_anyrom.sv
 $(PROJ_DIR)/tb/common_tb/top_tb_wrapper.sv
