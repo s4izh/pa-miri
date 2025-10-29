3bdd313abd2ec00d713c6f66f4641a9b71051ed8
s4izh <sergiosanz234@gmail.com>
2025-10-29 23:45:33 +0100

    feat: make verilator output fst file extension on waves

--- sim/scripts/verilator.mk
+++ sim/scripts/verilator.mk
@@ -5,7 +5,8 @@ COMPILE_FLAGS   := --cc --binary --build -O3 --trace-fst --timing
 COMPILED_FILE   := $(BUILD_DIR)/V$(TOP_MODULE)
 COMPILE_COMMAND := $(COMPILER) --Mdir $(BUILD_DIR) -f $(FILELIST) --top-module $(TOP_MODULE) $(COMPILE_FLAGS)
 
-WAVE_FILE       ?= $(BUILD_DIR)/waveform.vcd
+WAVE_EXTENSION  := fst
+WAVE_FILE       ?= $(BUILD_DIR)/waveform.$(WAVE_EXTENSION)
 SIMULATOR       := ./V$(TOP_MODULE)
 SIM_FLAGS       := +VCD_FILE=$(WAVE_FILE) $(PLUSARGS)
 VCD_FILE        := $(BUILD_DIR)/waveform.vcd
