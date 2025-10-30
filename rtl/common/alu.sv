2111ff8972f9fae2f21d024e28bb976de0d60977
SW1073 <eloiimerino@gmail.com>
2025-10-30 20:19:06 +0100

    fix: manual sra sign extension + XLEN_LOG2 parameter

--- rtl/common/alu.sv
+++ rtl/common/alu.sv
@@ -11,6 +11,7 @@ module alu#(
     input logic [3:0] alu_op_i,
     output logic [XLEN-1:0] result_o
 );
+    localparam XLEN_LOG2 = $clog2(XLEN);
     always_comb begin
         case (alu_op_i)
             ALU_ADD:  result_o = op1_i + op2_i;
@@ -20,9 +21,15 @@ module alu#(
             ALU_XOR:  result_o = op1_i ^ op2_i;
             ALU_SLT:  result_o = ($signed(op1_i) < $signed(op2_i)) ? 1 : 0;
             ALU_SLTU: result_o = (op1_i < op2_i) ? 1 : 0;
-            ALU_SLL:  result_o = op1_i << op2_i[4:0];
-            ALU_SRL:  result_o = op1_i >> op2_i[4:0];
-            ALU_SRA:  result_o = $signed(op1_i) >>> op2_i[4:0];
+            ALU_SLL:  result_o = op1_i << op2_i[XLEN_LOG2-1:0];
+            ALU_SRL:  result_o = op1_i >> op2_i[XLEN_LOG2-1:0];
+            ALU_SRA: begin
+                if (op2_i == XLEN) begin
+                    result_o = {XLEN{op1_i[XLEN-1]}};
+                end else begin
+                    result_o = (op1_i >> op2_i[4:0]) | ({XLEN{op1_i[XLEN-1]}} << (XLEN - op2_i[XLEN_LOG2-1:0]));
+                end
+            end
             default:  result_o = '0;
         endcase
     end
