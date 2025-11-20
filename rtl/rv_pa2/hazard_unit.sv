dafde7d75c4ffbc2ce50c98ad03e9c63dd45a181
SW1073 <eloiimerino@gmail.com>
2025-11-20 13:29:17 +0100

    fix: refactor rd_XX_zero to rd_XX_not_zero

--- rtl/rv_pa2/hazard_unit.sv
+++ rtl/rv_pa2/hazard_unit.sv
@@ -16,21 +16,21 @@ module hazard_unit (
     output logic stall_o
 );
 
-    logic rd_3e_zero, rd_4m_zero, rd_5w_zero;
+    logic rd_3e_not_zero, rd_4m_not_zero, rd_5w_not_zero;
 
-    assign rd_3e_zero = (rd_3e_i != '0);
-    assign rd_4m_zero = (rd_4m_i != '0);
-    assign rd_5w_zero = (rd_5w_i != '0);
+    assign rd_3e_not_zero = (rd_3e_i != '0);
+    assign rd_4m_not_zero = (rd_4m_i != '0);
+    assign rd_5w_not_zero = (rd_5w_i != '0);
 
     assign noop_o = jump_or_branch_3e_i;
     assign stall_o = (
-        ((rs1_2d_i == rd_3e_i) && rd_3e_zero && rs1_valid_2d_i && rd_is_wb_3e_i) ||
-        ((rs1_2d_i == rd_4m_i) && rd_4m_zero && rs1_valid_2d_i && rd_is_wb_4m_i) ||
-        ((rs1_2d_i == rd_5w_i) && rd_5w_zero && rs1_valid_2d_i && rd_is_wb_5w_i) ||
+        ((rs1_2d_i == rd_3e_i) && rd_3e_not_zero && rs1_valid_2d_i && rd_is_wb_3e_i) ||
+        ((rs1_2d_i == rd_4m_i) && rd_4m_not_zero && rs1_valid_2d_i && rd_is_wb_4m_i) ||
+        ((rs1_2d_i == rd_5w_i) && rd_5w_not_zero && rs1_valid_2d_i && rd_is_wb_5w_i) ||
 
-        ((rs2_2d_i == rd_3e_i) && rd_3e_zero && rs2_valid_2d_i && rd_is_wb_3e_i) ||
-        ((rs2_2d_i == rd_4m_i) && rd_4m_zero && rs2_valid_2d_i && rd_is_wb_4m_i) ||
-        ((rs2_2d_i == rd_5w_i) && rd_5w_zero && rs2_valid_2d_i && rd_is_wb_5w_i)
+        ((rs2_2d_i == rd_3e_i) && rd_3e_not_zero && rs2_valid_2d_i && rd_is_wb_3e_i) ||
+        ((rs2_2d_i == rd_4m_i) && rd_4m_not_zero && rs2_valid_2d_i && rd_is_wb_4m_i) ||
+        ((rs2_2d_i == rd_5w_i) && rd_5w_not_zero && rs2_valid_2d_i && rd_is_wb_5w_i)
     ) ? 1 : 0;
 
 endmodule
