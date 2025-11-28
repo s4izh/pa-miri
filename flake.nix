793c4f8ebe37da6f28fc3a245d2abf1aba0d2c2c
s4izh <sergiosanz234@gmail.com>
2025-11-28 01:53:39 +0100

    feat: package konata and izumi

--- flake.nix
+++ flake.nix
@@ -6,8 +6,12 @@
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
+    izumi = {
+      url = "git+https://github.com/Izumi-visualizer/izumi?submodules=1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, flake-utils, izumi, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
       python-pkgs = with pkgs; [
@@ -15,6 +19,73 @@
           ps.argcomplete
         ]))
       ];
+
+      konata-bin = pkgs.stdenv.mkDerivation rec {
+        pname = "konata";
+        version = "0.39";
+
+        src = pkgs.fetchzip {
+          url = "https://github.com/shioyadan/Konata/releases/download/v${version}/konata-linux-x64.tar.gz";
+          hash = "sha256-lwOFdDAs9pEceIyyCZYk2LSI53ti+j7GL/5nrmoyW1Y="; 
+          stripRoot = false;
+        };
+
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+
+        # installPhase = ''
+        #   mkdir -p $out/bin $out/share/konata
+        #   cp -r konata-linux-x64/resources/app.asar $out/share/konata/
+        #   makeWrapper ${pkgs.electron}/bin/electron $out/bin/konata \
+        #     --add-flags "$out/share/konata/app.asar" \
+        #     --add-flags "--no-sandbox"
+        # '';
+
+        installPhase = ''
+          mkdir -p $out/opt/konata
+          cp -r * $out/opt/konata/
+          chmod +x $out/opt/konata/konata-linux-x64/konata
+        '';
+      };
+
+      konata = pkgs.buildFHSEnv {
+        name = "konata";
+        targetPkgs = pkgs: (pkgs.appimageTools.defaultFhsEnvArgs.targetPkgs pkgs) ++ (with pkgs; [
+          udev
+          libxshmfence
+          glib
+          nss
+          nspr
+          at-spi2-atk
+          at-spi2-core
+          atk
+          cups
+          dbus
+          libdrm
+          pango
+          cairo
+          gtk3
+          xorg.libX11
+          xorg.libXcomposite
+          alsa-lib
+          expat
+        #   fontconfig
+        #   freetype
+        #   gdk-pixbuf
+        #   glib
+          libxkbcommon
+          xorg.libXdamage
+          xorg.libXext
+          xorg.libXfixes
+        #   xorg.libXi
+          xorg.libXrandr
+        #   xorg.libXrender
+        #   xorg.libXScrnSaver
+        #   xorg.libXtst
+          xorg.libxcb
+          libgbm
+        ]);
+        runScript = "${konata-bin}/opt/konata/konata-linux-x64/konata";
+      };
 
     in rec {
       devShell = pkgs.mkShell {
@@ -25,13 +96,16 @@
           iverilog
           verilator
           # gtkwave -- let engineers use their own version
-          # surfer --- let engineers use their own version
+          # surfer # --- let engineers use their own version
           svls
           universal-ctags
           python-pkgs
           sv-lang
           ninja
           lua
+
+          konata
+          izumi.packages.${system}.default
         ];
       };
     }
