261f8b0b7566cf47e8d2a3f30fd97b43d0b1f965
SW1073 <eloiimerino@gmail.com>
2025-12-29 21:03:42 +0100

    feat: parametrize benchmark/slides/memcpy.c

--- programs/benchmarks/slides/memcpy.c
+++ programs/benchmarks/slides/memcpy.c
@@ -1,6 +1,8 @@
+#define N 128
+
 int main() {
-    int a[128], b[128];
-    for (int i=0; i<128; i++) { a[i] = 5; }
-    for (int i=0; i<128; i++) { b[i] = a[i]; }
+    int a[N], b[N];
+    for (int i=0; i<N; i++) { a[i] = 5; }
+    for (int i=0; i<N; i++) { b[i] = a[i]; }
     return 0;
 }
