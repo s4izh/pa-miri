56d875d4199e41c7a074325c6fe4890a0c91646d
s4izh <sergiosanz234@gmail.com>
2026-01-12 21:49:16 +0100

    fix: build variable fixed so random paths don't appear on output

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,6 @@
 # This file must be sourced before anything
 export PROJ_DIR=$(pwd)
-export BUILD_DIR=$PROJ_DIR/build
+export BUILD_DIR=build
 export DOCKER_TOOLCHAIN_IMAGE="riscv-toolchain"
 export TOOLCHAIN_DIR="$PROJ_DIR/toolchains"
 export PROGRAMS_DIR="$PROJ_DIR/programs"
